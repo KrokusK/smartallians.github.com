829cee31eeec21af091a8186c51ff2cb94369358
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-09 15:34:01 +0600

    changed authorization functions 09.12.2019

--- construction/common/models/LoginForm.php
+++ construction/common/models/LoginForm.php
@@ -9,7 +9,12 @@ use yii\base\Model;
  */
 class LoginForm extends Model
 {
+    const STATUS_DELETED = 0;
+    const STATUS_INACTIVE = 9;
+    const STATUS_ACTIVE = 10;
+
     public $username;
+    public $email_phone;
     public $password;
     public $rememberMe = true;
 
@@ -22,8 +27,8 @@ class LoginForm extends Model
     public function rules()
     {
         return [
-            // username and password are both required
-            [['username', 'password'], 'required'],
+            // email_phone and password are both required
+            [['email_phone', 'password'], 'required'],
             // rememberMe must be a boolean value
             ['rememberMe', 'boolean'],
             // password is validated by validatePassword()
@@ -70,7 +75,16 @@ class LoginForm extends Model
     protected function getUser()
     {
         if ($this->_user === null) {
-            $this->_user = User::findByUsername($this->username);
+            //$this->_user = User::findByUsername($this->username);
+
+            if (preg_match("/^\+7\s\([0-9]{3}\)\s[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/", $this->email_phone)) {
+                $this->_user = User::findOne(['phone' => $this->email_phone, 'status' => self::STATUS_ACTIVE]);
+            }
+            elseif (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i", $this->email_phone)) {
+                $this->_user = User::findOne(['email' => $this->email_phone, 'status' => self::STATUS_ACTIVE]);
+            }
+
+
         }
 
         return $this->_user;
