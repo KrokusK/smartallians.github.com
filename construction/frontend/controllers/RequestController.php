0ccba9abdb46349c62b242f1fd9ce78bd9ec30a5
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-13 09:34:06 +0600

    Update RequestController.php

--- construction/frontend/controllers/RequestController.php
+++ construction/frontend/controllers/RequestController.php
@@ -427,7 +427,7 @@ class RequestController extends Controller
             $body = json_decode(Yii::$app->getRequest()->getRawBody(), true);
 
 
-            return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'success', 'message' => 'Успешно', var_dump($body)));
+            return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'success', 'message' => 'Успешно'));
         //} else {
         //    return Json::encode(array('method' => 'PUT', 'status' => '0', 'type' => 'error', 'message' => 'Ошибка'));
         //}
