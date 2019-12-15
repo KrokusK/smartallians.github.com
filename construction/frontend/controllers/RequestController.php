bfd2746e27f88b34e1818c3194a3979d1160e8fd
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-15 20:43:31 +0600

    Update RequestController.php

--- construction/frontend/controllers/RequestController.php
+++ construction/frontend/controllers/RequestController.php
@@ -449,9 +449,9 @@ class RequestController extends Controller
             }
 
             if ($modelRequest->validate()) {
-                return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'success', 'message' => 'Успешно', var_dump($bodyRaw), var_dump(ArrayHelper::toArray($modelRequest))));
+                return Json::encode(array('method' => 'PUT', 'status' => '0', 'type' => 'success', 'message' => 'Успешно', var_dump($bodyRaw), var_dump(ArrayHelper::toArray($modelRequest))));
             } else {
-                return Json::encode(array('method' => 'PUT', 'status' => '0', 'type' => 'error', 'message' => 'Ошибка'));
+                return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации'));
             }
 
     }
