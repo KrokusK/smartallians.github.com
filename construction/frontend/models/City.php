d8a924a6ce62205fd2b500d87f1428701a739f6e
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-16 23:06:52 +0600

    Update City.php

--- construction/frontend/models/City.php
+++ construction/frontend/models/City.php
@@ -27,6 +27,16 @@ class City extends \yii\db\ActiveRecord
         return [
             [['name'], 'required', 'message' => 'Поле должно быть заполнено'],
             [['id'], 'match', 'pattern' => '/^[0-9]*$/', 'message' => 'поле id  должно быть типа integer', 'skipOnEmpty' => true],
+            [['region_id'], 'in', 'range' =>
+                function ( $attribute, $params ) {
+                    $statusesRegionId = Region::find()->select(['id'])->asArray()->all();
+                    $statusesRegionIdStr = [];
+                    foreach ($statusesRegionId as $item) {
+                        array_push($statusesRegionIdStr, "{$item['id']}");
+                    }
+                    return $statusesRegionIdStr;
+                },
+                'message' => 'Регион не выбран из списка'],
             [['name'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
         ];
 
