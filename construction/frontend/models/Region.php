fcc9226e076765eb9a8778446d29a8d87a33ae3e
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-16 09:25:41 +0600

    api

---
+++ construction/frontend/models/Region.php
@@ -0,0 +1,51 @@
+<?php
+namespace frontend\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "status_request".
+ *
+ */
+class Region extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%region}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+
+        return [
+            [['name'], 'required', 'message' => 'Поле должно быть заполнено'],
+            [['name'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
+        ];
+
+    }
+
+    /**
+     *
+     * Link to table User_description
+     */
+    //public function getUserDescs()
+    //{
+    //    return $this->hasOne(UserDesc::className(), ['id' => 'user_desc_id']);
+    //}
+
+    /**
+     *
+     * Link to table Photo_ad
+     */
+    //public function getAdPhotos()
+    //{
+    //    return $this->hasMany(PhotoAd::className(), ['ad_id' => 'id']);
+    //}
+}
