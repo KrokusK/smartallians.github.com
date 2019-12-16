655b3a889747899003631e6fafbdfed5d060aa99
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-16 23:28:42 +0600

    Create models

--- construction/frontend/models/Attestation.php
+++ construction/frontend/models/Attestation.php
@@ -5,17 +5,17 @@ use Yii;
 use yii\db\ActiveRecord;
 
 /**
- * This is the model class for table "city".
+ * This is the model class for table "attestation".
  *
  */
-class City extends \yii\db\ActiveRecord
+class Attestation extends \yii\db\ActiveRecord
 {
     /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
-        return '{{%city}}';
+        return '{{%attestation}}';
     }
 
     /**
@@ -27,16 +27,6 @@ class City extends \yii\db\ActiveRecord
         return [
             [['name'], 'required', 'message' => 'Поле должно быть заполнено'],
             [['id'], 'match', 'pattern' => '/^[0-9]*$/', 'message' => 'поле id  должно быть типа integer', 'skipOnEmpty' => true],
-            [['region_id'], 'in', 'range' =>
-                function ( $attribute, $params ) {
-                    $statusesRegionId = Region::find()->select(['id'])->asArray()->all();
-                    $statusesRegionIdStr = [];
-                    foreach ($statusesRegionId as $item) {
-                        array_push($statusesRegionIdStr, "{$item['id']}");
-                    }
-                    return $statusesRegionIdStr;
-                },
-                'message' => 'Регион не выбран из списка'],
             [['name'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
         ];
 
