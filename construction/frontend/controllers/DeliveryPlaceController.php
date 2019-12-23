53c3ec01f4b86c1b8e983c3ffe422981aa577567
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-23 15:31:55 +0600

    api

---
+++ construction/frontend/controllers/DeliveryPlaceController.php
@@ -0,0 +1,431 @@
+<?php
+namespace frontend\controllers;
+
+use frontend\models\DeliveryPlace;
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+/**
+ * API DeliveryPlace controller
+ */
+class DeliveryPlaceController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+
+    /**
+     * GET Method. DeliveryPlace table.
+     * Get records by parameters
+     *
+     * @return json
+     */
+    public function actionView()
+    {
+        // check user is a guest
+        if (Yii::$app->user->isGuest) {
+            //return $this->goHome();
+        }
+
+        //if (Yii::$app->request->isAjax) {
+        //GET data from GET request
+        $model = new DeliveryPlace();
+        if ($model->load(Yii::$app->request->get())) {
+
+            // Search record by parametrs in the database
+            $query = DeliveryPlace::find();
+            foreach (ArrayHelper::toArray($model) as $key => $value) {
+                $query->andWhere([$key => $value]);
+            }
+
+            $modelDeliveryPlace = $query->orderBy('name')->all();
+
+            // get properties from DeliveryPlace object
+            $DeliveryPlaceResponse = array('method' => 'GET', 'status' => '0', 'type' => 'success');
+            array_push($DeliveryPlaceResponse, ArrayHelper::toArray($modelDeliveryPlace));
+
+            return Json::encode($DeliveryPlaceResponse);
+
+        } else {
+            // Search all records in the database
+            $query = DeliveryPlace::find();
+
+            $modelDeliveryPlace = $query->orderBy('name')->all();
+
+            // get properties from DeliveryPlace object
+            $DeliveryPlaceResponse = array('method' => 'GET', 'status' => '0', 'type' => 'success');
+            array_push($DeliveryPlaceResponse, ArrayHelper::toArray($modelDeliveryPlace));
+
+            return Json::encode($DeliveryPlaceResponse);
+        }
+        //}
+    }
+
+
+    /**
+     * POST Method. DeliveryPlace table.
+     * Insert records by parameters
+     *
+     * @return json
+     */
+    public function actionCreate()
+    {
+        // check user is a guest
+        if (Yii::$app->user->isGuest) {
+            //return $this->goHome();
+        }
+
+        //if (Yii::$app->request->isAjax) {
+        //GET data from body request
+        //Yii::$app->request->getBodyParams()
+        $fh = fopen("php://input", 'r');
+        $put_string = stream_get_contents($fh);
+        $put_string = urldecode($put_string);
+        //$array_put = $this->parsingDeliveryPlaceFormData($put_string);
+
+        $bodyRaw = json_decode(Yii::$app->getRequest()->getRawBody(), true);
+        //$body = json_decode(Yii::$app->getRequest()->getBodyParams(), true);
+
+        //$modelDeliveryPlace->setAttributes($bodyRaw);
+
+        // load attributes in DeliveryPlace object
+        // example: yiisoft/yii2/base/Model.php
+        if (is_array($bodyRaw)) {
+            if (array_key_exists('DeliveryPlace[id]', $bodyRaw)) {
+                return Json::encode(array('method' => 'POST', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка: Недопустимый параметр: id'));
+            } else {
+                $modelDeliveryPlace = new DeliveryPlace();
+
+                // fill in the properties in the DeliveryPlace object
+                foreach ($bodyRaw as $name => $value) {
+                    $pos_begin = strpos($name, '[') + 1;
+                    if (strtolower(substr($name, 0, $pos_begin - 1)) != 'deliveryplace') return Json::encode(array('method' => 'POST', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации: '.$name));
+                    $pos_end = strpos($name, ']');
+                    $name = substr($name, $pos_begin, $pos_end-$pos_begin);
+                    //if (isset($modelDeliveryPlace->$name)) {
+                    //    $modelDeliveryPlace->$name = $value;
+                    //}
+                    //if (property_exists($modelDeliveryPlace, $name)) {
+                    if ($modelDeliveryPlace->hasAttribute($name)) {
+                        if ($name != 'id') $modelDeliveryPlace->$name = $value;
+                    }
+                }
+            }
+
+
+        }
+
+        if ($modelDeliveryPlace->validate()) {
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                $flag = $modelDeliveryPlace->save(false); // insert
+
+                if ($flag == true) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                    return Json::encode(array('method' => 'POST', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка: Место поставки не может быть сохранено'));
+                }
+            } catch (Exception $ex) {
+                $transaction->rollBack();
+                return Json::encode(array('method' => 'POST', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка: Место поставки не может быть сохранено'));
+            }
+
+            //return Json::encode(array('method' => 'POST', 'status' => '0', 'type' => 'success', 'message' => 'Место поставки успешно сохранено', var_dump($bodyRaw), var_dump(ArrayHelper::toArray($modelDeliveryPlace))));
+            return Json::encode(array('method' => 'POST', 'status' => '0', 'type' => 'success', 'message' => 'Место поставки успешно сохранено'));
+        } else {
+            return Json::encode(array('method' => 'POST', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации'));
+        }
+        //}
+    }
+
+
+    /**
+     * PUT, PATCH Method. DeliveryPlace table.
+     * Update records by parameters
+     *
+     * @return json
+     */
+    public function actionUpdate()
+    {
+        // check user is a guest
+        if (Yii::$app->user->isGuest) {
+            //return $this->goHome();
+        }
+
+        //if (Yii::$app->request->isAjax) {
+            //GET data from body request
+            //Yii::$app->request->getBodyParams()
+            $fh = fopen("php://input", 'r');
+            $put_string = stream_get_contents($fh);
+            $put_string = urldecode($put_string);
+            //$array_put = $this->parsingDeliveryPlaceFormData($put_string);
+
+            $bodyRaw = json_decode(Yii::$app->getRequest()->getRawBody(), true);
+            //$body = json_decode(Yii::$app->getRequest()->getBodyParams(), true);
+
+            //$modelDeliveryPlace->setAttributes($bodyRaw);
+
+            // load attributes in DeliveryPlace object
+            // example: yiisoft/yii2/base/Model.php
+            if (is_array($bodyRaw)) {
+                if (array_key_exists('DeliveryPlace[id]', $bodyRaw)) {
+                    // check input parametrs
+                    if (!preg_match("/^[0-9]*$/",$bodyRaw['DeliveryPlace[id]'])) {
+                        return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации: id'));
+                    }
+
+                    // Search record by id in the database
+                    $query = DeliveryPlace::find()
+                        ->where(['id' => $bodyRaw['DeliveryPlace[id]']]);
+                    //->where(['AND', ['id' => $modelDeliveryPlace->id], ['user_desc_id'=> $var2]]);
+
+                    $modelDeliveryPlace = $query->orderBy('name')->one();
+
+                    if (!empty($modelDeliveryPlace)) {
+                        // update in the properties in the DeliveryPlace object
+                        foreach ($bodyRaw as $name => $value) {
+                            $pos_begin = strpos($name, '[') + 1;
+                            if (strtolower(substr($name, 0, $pos_begin - 1)) != 'deliveryplace') return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации: '.$name));
+                            $pos_end = strpos($name, ']');
+                            $name = substr($name, $pos_begin, $pos_end - $pos_begin);
+
+                            if ($name != 'id') $modelDeliveryPlace->$name = $value;
+                        }
+                    } else {
+                        return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации: id'));
+                    }
+                } else {
+                    $modelDeliveryPlace = new DeliveryPlace();
+
+                    // fill in the properties in the DeliveryPlace object
+                    foreach ($bodyRaw as $name => $value) {
+                        $pos_begin = strpos($name, '[') + 1;
+                        if (strtolower(substr($name, 0, $pos_begin - 1)) != 'deliveryplace') return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации: '.$name));
+                        $pos_end = strpos($name, ']');
+                        $name = substr($name, $pos_begin, $pos_end-$pos_begin);
+                        //if (isset($modelDeliveryPlace->$name)) {
+                        //    $modelDeliveryPlace->$name = $value;
+                        //}
+                        //if (property_exists($modelDeliveryPlace, $name)) {
+                        if ($modelDeliveryPlace->hasAttribute($name)) {
+                            if ($name != 'id') $modelDeliveryPlace->$name = $value;
+                        }
+                    }
+                }
+
+
+            }
+
+            if ($modelDeliveryPlace->validate()) {
+                $transaction = \Yii::$app->db->beginTransaction();
+                try {
+                    $flag = $modelDeliveryPlace->save(false); // insert
+
+                    if ($flag == true) {
+                        $transaction->commit();
+                    } else {
+                        $transaction->rollBack();
+                        return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка: Место поставки не может быть сохранено (обновлено)'));
+                    }
+                } catch (Exception $ex) {
+                    $transaction->rollBack();
+                    return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка: Место поставки не может быть сохранено (обновлено)'));
+                }
+
+                //return Json::encode(array('method' => 'PUT', 'status' => '0', 'type' => 'success', 'message' => 'Место поставки успешно сохранено (обновлено)', var_dump($bodyRaw), var_dump(ArrayHelper::toArray($modelDeliveryPlace))));
+                return Json::encode(array('method' => 'PUT', 'status' => '0', 'type' => 'success', 'message' => 'Место поставки успешно сохранено (обновлено)'));
+            } else {
+                return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации'));
+            }
+        //}
+    }
+
+
+    /**
+     * DELETE Method. DeliveryPlace table.
+     * Delete records by parameters
+     *
+     * @return json
+     */
+    public function actionDelete()
+    {
+        // check user is a guest
+        if (Yii::$app->user->isGuest) {
+            //return $this->goHome();
+        }
+
+        //if (Yii::$app->request->isAjax) {
+        //GET data from body request
+        //Yii::$app->request->getBodyParams()
+        $fh = fopen("php://input", 'r');
+        $put_string = stream_get_contents($fh);
+        $put_string = urldecode($put_string);
+        //$array_put = $this->parsingDeliveryPlaceFormData($put_string);
+
+        $bodyRaw = json_decode(Yii::$app->getRequest()->getRawBody(), true);
+        //$body = json_decode(Yii::$app->getRequest()->getBodyParams(), true);
+
+        //$modelDeliveryPlace->setAttributes($bodyRaw);
+
+        // load attributes in DeliveryPlace object
+        // example: yiisoft/yii2/base/Model.php
+        if (is_array($bodyRaw)) {
+            if (array_key_exists('DeliveryPlace[id]', $bodyRaw)) {
+                // check input parametrs
+                if (!preg_match("/^[0-9]*$/",$bodyRaw['DeliveryPlace[id]'])) {
+                    return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка валидации: id'));
+                }
+
+                // Search record by id in the database
+                $query = DeliveryPlace::find()
+                    ->where(['id' => $bodyRaw['DeliveryPlace[id]']]);
+                //->where(['AND', ['id' => $modelDeliveryPlace->id], ['user_desc_id'=> $var2]]);
+
+                $modelDeliveryPlace = $query->orderBy('name')->one();
+            }
+        }
+
+        if (!empty($modelDeliveryPlace)) {
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                $flag = $modelDeliveryPlace->delete($bodyRaw['DeliveryPlace[id]']); // delete
+
+                if ($flag == true) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                    return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка: Место поставки не может быть удалено'));
+                }
+            } catch (Exception $ex) {
+                $transaction->rollBack();
+                return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка: Место поставки не может быть удалено'));
+            }
+
+            //return Json::encode(array('method' => 'PUT', 'status' => '0', 'type' => 'success', 'message' => 'Место поставки успешно удалено', var_dump($bodyRaw), var_dump(ArrayHelper::toArray($modelDeliveryPlace))));
+            return Json::encode(array('method' => 'PUT', 'status' => '0', 'type' => 'success', 'message' => 'Место поставки успешно удалено'));
+        } else {
+            return Json::encode(array('method' => 'PUT', 'status' => '1', 'type' => 'error', 'message' => 'Ошибка: Место поставки не может быть удалено'));
+        }
+        //}
+    }
+
+
+    /**
+     * Parsing request Method.
+     *
+     * @return array
+     */
+    public function parsingDeliveryPlaceFormData($put_string)
+    {
+        //            //$put_string = json_decode($put_string_json, TRUE);
+        //            //$put_string=Yii::$app->request->getBodyParams();
+        //
+        //            //$temp = json_decode($put_string, TRUE);
+        //
+        //            //$put_param = explode("&", $put_string);
+        //            //$array_put=array();
+        //            //parse_str($put_string, $array_put);
+        //
+        //
+        //
+        //            //foreach($array_put as $put_val)
+        //            //{
+        //            //    $param = explode("=", $put_val);
+        //            //    $array_put[$paam[0]]=urldecode($param[1]);
+        //}
+
+        //$request = Yii::$app->request;
+
+        // returns all parameters
+        //$params = $request->getBodyParams();
+
+        //name=\"Request[name]\"\r\n\r\ntest\r\n-----------------------------4833311154639"
+
+        // returns the parameter "id"
+        //$param = $request->getBodyParam('nad');
+
+        function strpos_recursive($haystack, $needle, $offset = 0, &$results = array()) {
+            $offset = strpos($haystack, $needle, $offset);
+            if($offset === false) {
+                return $results;
+            } else {
+                $results[] = $offset;
+                return strpos_recursive($haystack, $needle, ($offset + 1), $results);
+            }
+        }
+
+        $string = $put_string;
+        $search = 'name="';
+        $found = strpos_recursive($string, $search);
+
+        if($found) {
+            foreach($found as $pos) {
+                //$temp = 'Found "'.$search.'" in string "'.$string.'" at position '.$pos;
+
+                $key = substr($string, ($pos + strlen($search)), (strpos($string, '"', ($pos + strlen($search))) - ($pos + strlen($search))));
+
+                $pos_begin = strpos($string, '"', ($pos + strlen($search))) + 4;
+                $pos_end = strpos($string, '-', $pos_begin) - 2;
+                $value = substr($string, $pos_begin + 1, $pos_end - ($pos_begin + 1));
+
+                $array_put[$key] = $value;
+            }
+        } else {
+            //$temp = '"'.$search.'" not found in "'.$string.'"';
+        }
+
+        return $array_put;
+    }
+}
