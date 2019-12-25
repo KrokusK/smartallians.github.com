94a4f3bf4a11fab50a67676f44a28aa66b2a563b
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-25 12:03:13 +0600

    frontend

--- construction/frontend/assets/AppAsset.php
+++ construction/frontend/assets/AppAsset.php
@@ -13,8 +13,21 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/chunk-01dd175a.72393b08.css',
+        'css/chunk-5fed29bc.bc26259b.css',
+        'css/chunk-64270723.8e63a075.css',
+        'css/chunk-b5aa61f8.a6b9f33d.css',
+        'css/chunk-c6d5b5d2.207dbab3.css',
+        'css/app.d33537c5.css',
     ];
     public $js = [
+        'js/chunk-01dd175a.9171cbe4.js',
+        'js/chunk-5fed29bc.b5434ad0.js',
+        'js/chunk-64270723.c945e296.js',
+        'js/chunk-b5aa61f8.811fdaea.js',
+        'js/chunk-c6d5b5d2.fde16e82.js',
+        'js/app.4d4a80ed.js',
+        'js/chunk-vendors.29d4b946.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- construction/frontend/views/site/login.php
+++ construction/frontend/views/site/login.php
@@ -11,31 +11,10 @@ $this->title = 'Авторизация';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Пожалуйста заполните ниже поля для авторизации:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'email_phone')->textInput(['autofocus' => true])->hint('Пожалуйста, введите email или телефон в формате: +7 (999) 999-99-99')->label('Email / Телефон в формате: +7 (999) 999-99-99') ?>
-
-                <?= $form->field($model, 'password')->passwordInput()->hint('Пожалуйста, введите пароль')->label('Пароль') ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox()->label('Запомнить меня') ?>
-
-                <div style="color:#999;margin:1em 0">
-                    Если Вы забыли свой пароль, то перейдите по ссылке <?= Html::a('сбросить пароль', ['site/request-password-reset']) ?>.
-                    <br>
-                    Проверить почту? <?= Html::a('выслать ключ верификации', ['site/resend-verification-email']) ?>
-                </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Вход', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
+    <noscript>
+        <strong>We're sorry but smart-alliance doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+    </noscript>
+    <div id=app></div>
+    <script src=/js/chunk-vendors.29d4b946.js></script>
+    <script src=/js/app.4d4a80ed.js></script>
 </div>
