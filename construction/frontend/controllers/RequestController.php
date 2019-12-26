6fbabd44f9034ae0f90c76ab78bc2f2a1fd4eafd
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-26 16:12:22 +0600

    Update RequestController.php

--- construction/frontend/controllers/RequestController.php
+++ construction/frontend/controllers/RequestController.php
@@ -108,13 +108,10 @@ class RequestController extends Controller
                 ->asArray()
                 ->all();
 
-            // get properties from Request object
+            // get properties from Request object and from links
             $RequestResponse = array('method' => 'GET', 'status' => '0', 'type' => 'success');
-            //'kind_job_id' => $modelRequest['kindJob']['id'], 'kind_job_name' => $modelRequest['kindJob']['name']
             array_push($RequestResponse, ArrayHelper::toArray($modelRequest));
-            //array_push($RequestResponse, var_dump($modelRequest['kindJob']['name']));
             //array_push($RequestResponse, var_dump($modelRequest));
-
 
             return Json::encode($RequestResponse);
 
