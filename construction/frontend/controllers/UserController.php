638bdf65ca72366482322b64246789f096e179f8
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-26 17:26:00 +0600

    api

---
+++ construction/frontend/controllers/UserController.php
@@ -0,0 +1,94 @@
+<?php
+namespace frontend\controllers;
+
+use frontend\models\Profile;
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+/**
+ * User controller
+ */
+class UserController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                    //'login' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Get a user data.
+     *
+     * @return json
+     */
+    public function actionData()
+    {
+        // check user is a guest
+        if (!Yii::$app->user->isGuest && Yii::$app->request->isPost) {
+            //return $this->goHome();
+
+            // Get user data from tables
+            $query = Profile::find()
+                ->where(['user_id' => Yii::$app->user->getId()]);
+            $userData = $query->orderBy('id')
+                ->with('users')
+                ->asArray()
+                ->one();
+
+            return Json::encode(array('method' => 'POST', 'status' => 0, 'type' => 'success', 'message' => 'Вы уже авторизованы!', 'id_user' => Yii::$app->user->getId(), 'id_profile' => $userData['id'], 'id_kind_user' => $userData['kind_user_id'], 'fio' => $userData['fio'], 'username' => $userData['users']['username'], 'email' => $userData['users']['email'], 'avatar' => $userData['avatar']));
+        } else {
+            return Json::encode(array('status' => 1, 'type' => 'error', 'message' => 'Ошибка!');
+        }
+    }
+
+
+}
