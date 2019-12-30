369c3da9f0e9ecca9cf7f34d9bb720920fb8adf8
KrokusK <55874907+KrokusK@users.noreply.github.com>
2019-12-30 14:46:32 +0600

    Update RequestController.php

--- construction/frontend/controllers/RequestController.php
+++ construction/frontend/controllers/RequestController.php
@@ -209,7 +209,7 @@ class RequestController extends Controller
         }
 
         $RequestResponse = array('method' => 'GET', 'status' => 0, 'type' => 'success');
-        array_push($RequestResponse, ArrayHelper::toArray($modelRequestKindJob));
+        array_push($RequestResponse, ArrayHelper::toArray($modelRequest));
         return Json::encode($RequestResponse);
 
         if ($modelRequest->validate() && $modelRequestKindJob->validate('kind_job_id')) {
