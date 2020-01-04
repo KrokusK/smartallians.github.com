76c09bef3515069e2e7016b94da421655ffe7617
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-01-04 07:53:16 +0600

    modules

---
+++ construction/frontend/modules/v1/Module.php
@@ -0,0 +1,12 @@
+<?php
+namespace fontend\modules\v1;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'frontend\modules\v1\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+}
---
+++ construction/frontend/modules/v1/models/City.php
@@ -0,0 +1,90 @@
+<?php
+namespace frontend\modules\v1\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "city".
+ *
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%city}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+
+        return [
+            [['name', 'region_id'], 'required', 'message' => 'Поле должно быть заполнено'],
+            [['id'], 'match', 'pattern' => '/^[0-9]*$/', 'message' => 'поле id  должно быть типа integer', 'skipOnEmpty' => true],
+            [['region_id'], 'in', 'range' =>
+                function ( $attribute, $params ) {
+                    $statusesRegionId = Region::find()->select(['id'])->asArray()->all();
+                    $statusesRegionIdStr = [];
+                    foreach ($statusesRegionId as $item) {
+                        array_push($statusesRegionIdStr, "{$item['id']}");
+                    }
+                    return $statusesRegionIdStr;
+                },
+                'message' => 'Регион не выбран из списка'],
+            [['name'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
+        ];
+
+    }
+
+    /**
+     *
+     * Link to table profile
+     */
+    public function getProfiles()
+    {
+        return $this->hasMany(Profile::className(), ['id' => 'profile_id'])
+            ->viaTable('profile_city', ['city_id' => 'id']);
+    }
+
+    /**
+     *
+     * Link to table region
+     */
+    public function getRegions()
+    {
+        return $this->hasOne(Region::className(), ['id' => 'region_id']);
+    }
+
+    /**
+     *
+     * Link to table request
+     */
+    public function getRequests()
+    {
+        return $this->hasOne(Request::className(), ['city_id' => 'id']);
+    }
+
+    /**
+     *
+     * Link to table delivery_place
+     */
+    public function getDeliveryPlace()
+    {
+        return $this->hasOne(DeliveryPlace::className(), ['city_id' => 'id']);
+    }
+
+    /**
+     *
+     * Link to table departure_place
+     */
+    public function getDeparturePlace()
+    {
+        return $this->hasOne(DeparturePlace::className(), ['city_id' => 'id']);
+    }
+}
