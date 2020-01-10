8018a456ddc6372d0368d28165ce45af83d574a7
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-01-10 16:44:01 +0600

    Update PhotoController.php

--- construction/api/modules/v2/controllers/PhotoController.php
+++ construction/api/modules/v2/controllers/PhotoController.php
@@ -182,7 +182,7 @@ class PhotoController extends Controller
             */
 
             //$modelPhoto->imageFiles = $postParams['imagefiles'];
-            $modelPhoto->load(Yii::$app->getRequest()->post());
+            $modelPhoto->load(Yii::$app->getRequest()->post('request_id'));
 
             $modelPhoto->imageFiles = UploadedFile::getInstances($modelPhoto, 'imageFiles');
             if ($modelPhoto->upload()) { // save ad photos
