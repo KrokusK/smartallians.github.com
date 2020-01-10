006e659a5d6e6d98b0a3f31e261876585f87bb6b
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-01-10 14:42:22 +0600

    Update PhotoController.php

--- construction/api/modules/v2/controllers/PhotoController.php
+++ construction/api/modules/v2/controllers/PhotoController.php
@@ -178,9 +178,9 @@ class PhotoController extends Controller
                 }
             }
 
-            $PhotoResponse = array('method' => 'POST', 'status' => 0, 'type' => 'test');
-            array_push($PhotoResponse, ArrayHelper::toArray($modelPhoto));
-            return Json::encode($PhotoResponse);
+            //$PhotoResponse = array('method' => 'POST', 'status' => 0, 'type' => 'test');
+            //array_push($PhotoResponse, ArrayHelper::toArray($modelPhoto));
+            //return Json::encode($PhotoResponse);
 
             if ($modelPhoto->validate()) {
             /*
