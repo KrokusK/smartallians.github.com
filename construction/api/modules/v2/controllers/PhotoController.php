ff9f00349ad4189aabd7b0bea4283f3a23abb98d
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-01-11 02:10:30 +0600

    Update PhotoController.php

--- construction/api/modules/v2/controllers/PhotoController.php
+++ construction/api/modules/v2/controllers/PhotoController.php
@@ -192,6 +192,9 @@ class PhotoController extends Controller
                         }
 
                         $modelPhotoFile->path = '/uploads/photo/'.$file;
+                        // get properties from Photo object and from links
+                        $PhotoResponse = array('method' => 'POST', 'status' => 0, 'type' => 'test');
+                        array_push($PhotoResponse, ArrayHelper::toArray($modelPhotoFile));
                         if ($modelPhotoFile->validate()) {
                             $flagPhoto = $modelPhotoFile->save(false); // insert
                         } else {
