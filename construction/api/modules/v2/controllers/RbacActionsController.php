1b1e465e909297481db85ea941f04e2bbce429f3
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-01-15 15:30:14 +0600

    rbac

--- construction/api/modules/v2/controllers/RbacActionsController.php
+++ construction/api/modules/v2/controllers/RbacActionsController.php
@@ -1,6 +1,7 @@
 <?php
 namespace api\modules\v2\controllers;
 
+use api\modules\v2\models\Request;
 use api\modules\v2\rbac\CustomerRule;
 //use yii\console\rbac;
 
@@ -45,21 +46,51 @@ class RbacActionsController extends Controller
         ];
     }
 
+
     /**
-     * GET Method. RBAC table.
-     * Get records by parameters
+     * POST Method. RBAC table.
+     * Check user by createCustomer permission
      *
      * @return json
      */
     public function actionCustomerCreate()
     {
-        $getParams = Yii::$app->getRequest()->get();
+        $postParams = Yii::$app->getRequest()->post();
 
         // check user is a guest
-        if (empty(\Yii::$app->user->loginByAccessToken($getParams['token']))) {
+        if (empty(\Yii::$app->user->loginByAccessToken($postParams['token']))) {
             //return $this->goHome();
             return Json::encode(array('method' => 'GET', 'status' => 1, 'type' => 'error', 'message' => 'Ошибка: Аутентификация не выполнена'));
         }
+
+        if (\Yii::$app->user->can('createCustomer')) {
+            return Json::encode(array('method' => 'GET', 'status' => 0, 'type' => 'success', 'message' => 'Успешно: Вы можете добавлять записи в таблицы Заказчика'));
+        } else {
+            return Json::encode(array('method' => 'GET', 'status' => 1, 'type' => 'error', 'message' => 'Ошибка: Вы не можете добавлять записи в таблицы Заказчика'));
+        }
+    }
+
+
+    /**
+     * POST Method. RBAC table.
+     * Check user by updateOwnCustomer permission
+     *
+     * @return json
+     */
+    public function actionCustomerOwnUpdate()
+    {
+        $postParams = Yii::$app->getRequest()->post();
+
+        // check user is a guest
+        if (empty(\Yii::$app->user->loginByAccessToken($postParams['token']))) {
+            //return $this->goHome();
+            return Json::encode(array('method' => 'GET', 'status' => 1, 'type' => 'error', 'message' => 'Ошибка: Аутентификация не выполнена'));
+        }
+
+        // Search record by id in the database
+        $queryRequest = Request::find()
+            ->where(['id' => $postParams['id']]);
+        $modelRequest = $queryRequest->one();
 
         if (\Yii::$app->user->can('createCustomer')) {
             return Json::encode(array('method' => 'GET', 'status' => 0, 'type' => 'success', 'message' => 'Успешно: Вы можете добавлять записи в таблицы Заказчика'));
