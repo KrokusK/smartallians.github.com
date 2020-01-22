5498a80e018c75e63b3397540baf14efcd0372a0
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-01-22 10:30:26 +0600

    Update RequestController.php

--- construction/api/modules/v2/controllers/RequestController.php
+++ construction/api/modules/v2/controllers/RequestController.php
@@ -1,7 +1,6 @@
 <?php
 namespace api\modules\v2\controllers;
 
-use api\modules\v2\models\KindJob;
 use api\modules\v2\models\Profile;
 use api\modules\v2\models\ProfileRROD;
 use api\modules\v2\models\Request;
@@ -116,7 +115,7 @@ class RequestController extends Controller
         if (count($getParams) > 0) {
             // Because the field names may match within a single query, the parameter names may not match the table field names. To solve this problem let's create an associative arrays
             $arrayRequestAssoc = array ('id' => 'id', 'status_request_id' => 'status_request_id', 'city_id' => 'city_id', 'address' => 'address', 'name' => 'name', 'description' => 'description', 'task' => 'task', 'budjet' => 'budjet', 'period' => 'period', 'date_begin' => 'date_begin', 'date_end' => 'date_end');
-            $arrayKindJobAssoc = array ('id' => 'work_type');
+            $arrayKindJobAssoc = array ('kind_job_id' => 'work_type');
 
             // Search record by id in the database
             if (in_array('admin', $userRole)) {
@@ -139,8 +138,8 @@ class RequestController extends Controller
                     }
                 }
             }
-            /*
-            $modelValidate = new KindJob();
+
+            $modelValidate = new RequestKindJob();
             foreach ($arrayKindJobAssoc as $nameKindJobAssoc => $valueKindJobAssoc) {
                 if (array_key_exists($valueKindJobAssoc, $getParams)) {
                     if ($modelValidate->hasAttribute($nameKindJobAssoc)) {
@@ -151,7 +150,7 @@ class RequestController extends Controller
                     }
                 }
             }
-            */
+
 
             $modelRequest = $query->orderBy('created_at')
                 ->with('kindJob')
