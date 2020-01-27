f290b77f28cb74c282ac4664439d934cbc90e9f4
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-01-28 00:41:53 +0600

    Update StatusDeliveryController.php

--- construction/api/modules/v2/controllers/StatusDeliveryController.php
+++ construction/api/modules/v2/controllers/StatusDeliveryController.php
@@ -70,12 +70,12 @@ class StatusDeliveryController extends Controller
      * @return json
      */
     public function actionView()
-    {
-        return Json::encode(array('method' => 'GET', 'status' => 1, 'type' => 'error', 'message' => Yii::$app->getRequest()->getMethod()));
-
+    {     
         $modelStatusDelivery = new StatusDelivery();
         $modelStatusDelivery->setMethodAndParams();
         $getParams = $modelStatusDelivery->getRequestParams();
+
+        return Json::encode(array('method' => 'GET', 'status' => 1, 'type' => 'error', 'message' => var_dump($getParams)));
 
         /*
         try {
