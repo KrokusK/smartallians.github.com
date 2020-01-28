f800a8827972078e1b8015b6c71e24f1ff22e609
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-01-28 17:07:45 +0600

    Update StatusDelivery.php

--- construction/api/modules/v2/models/StatusDelivery.php
+++ construction/api/modules/v2/models/StatusDelivery.php
@@ -3,8 +3,6 @@ namespace api\modules\v2\models;
 
 use Yii;
 use yii\db\ActiveRecord;
-use yii\helpers\Json;
-use yii\web\BadRequestHttpException;
 
 /**
  * This is the model class for table "status_delivery".
@@ -12,14 +10,6 @@ use yii\web\BadRequestHttpException;
  */
 class StatusDelivery extends \yii\db\ActiveRecord
 {
-    /**
-     * properties
-     */
-    protected $method;
-    protected $message;
-    protected $userByToken;
-    protected $params;
-
      /**
      * {@inheritdoc}
      */
@@ -46,108 +36,6 @@ class StatusDelivery extends \yii\db\ActiveRecord
             [['name'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
         ];
 
-    }
-
-    /**
-     * init
-     */
-
-    public function init()
-    {
-        parent::init();
-
-        // Set properties: method, params
-        $this->setProperties();
-    }
-
-    /**
-     * Get params from request
-     *
-     */
-
-    public function getRequestParams()
-    {
-        return $this->params;
-    }
-
-    /**
-     * Defining request method and
-     * set params by values from request
-     */
-
-    public function setProperties()
-    {
-        $this->method = strtolower(Yii::$app->getRequest()->getMethod());
-        $this->setParamsByMethod();
-    }
-
-    /**
-     * Set params from request
-     *
-     */
-
-    public function setParamsByMethod()
-    {
-        switch ($this->method) {
-            case 'get':
-                $this->params = Yii::$app->getRequest()->get();
-                break;
-            case 'post':
-                $this->params = Yii::$app->getRequest()->post();
-                break;
-            case 'put':
-            case 'patch':
-            case 'delete':
-                $this->params = json_decode(Yii::$app->getRequest()->getRawBody(), true);
-        }
-    }
-
-    /**
-     * Authorization user by token in params
-     */
-    public function loginByParams()
-    {
-        if (array_key_exists('token', $this->params)) {
-            $this->userByToken = \Yii::$app->user->loginByAccessToken($this->params['token']);
-            if (!empty($this->userByToken)) {
-                return $this->userByToken;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Set text message
-     */
-    public function setMessage($status = 1, $message = '') {
-        switch ($status) {
-            case 0:
-                $this->message = [
-                    'method' => $this->method,
-                    'status' => $status,
-                    'type' => 'success',
-                    'message' => $message
-                ];
-                break;
-            case 1:
-                $this->message = [
-                    'method' => $this->method,
-                    'status' => $status,
-                    'type' => 'error',
-                    'message' => $message
-                ];
-        }
-    }
-
-    /**
-     * Get text message
-     *
-     */
-
-    public function getMessage()
-    {
-        return $this->message;
     }
 
     /**
