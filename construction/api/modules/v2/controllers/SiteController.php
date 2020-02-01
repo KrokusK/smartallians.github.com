fc6a714a3edc545e0aa759dd49bfea91e56e79b6
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-02-01 22:04:26 +0600

    cors

---
+++ construction/api/modules/v2/controllers/SiteController.php
@@ -0,0 +1,73 @@
+<?php
+namespace api\modules\v2\controllers;
+
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['options'],
+                'rules' => [
+                    [
+                        'actions' => ['options'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'options' => ['options'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Don't response on the OPTIONS request.
+     *
+     * @return nothing
+     */
+    public function actionOptions()
+    {
+        // do nothing
+
+        // test
+        //return Json::encode(array('method' => 'OPTIONS', 'status' => 0, 'type' => 'success'));
+    }
+}
