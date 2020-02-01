4b6f2ed7fd155b8be54a77fdc81dffcf6c3a2b43
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-02-01 20:18:49 +0600

    Update ResponseController.php

--- construction/api/modules/v2/controllers/ResponseController.php
+++ construction/api/modules/v2/controllers/ResponseController.php
@@ -211,7 +211,7 @@ class ResponseController extends Controller
                 'period' => $modelResponse[0]['period'],
                 'profile_id' => $modelResponse[0]['profiles'][0]['id'],
                 'fio' => $modelResponse[0]['profiles'][0]['fio'],
-                //'firm_name' => $modelResponse[0]['profiles']['firm_name'],
+                'firm_name' => $modelResponse[0]['profiles']['firm_name'],
                 'avatar' => $modelResponse[0]['profiles'][0]['avatar']
             );
             array_push($RequestResponse, ArrayHelper::toArray($modelResponse));
