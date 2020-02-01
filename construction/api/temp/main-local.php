fdea76f7bf4cede1195a9620b015b80a14574dde
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-02-01 21:39:09 +0600

    cors

--- construction/api/temp/main-local.php
+++ construction/api/temp/main-local.php
@@ -18,6 +18,7 @@ $config = [
             'enableStrictParsing' => false,
             'rules' => [
                 // Route OPTIONS request
+                'OPTIONS /<module:\w+>/<controller:(region|city|specialization|profile|type-job|kind-user|contractor|attestation|portfolio|position|kind-job|request|response|status-request|status-response|photo|status-feedback|feedback|status-completion|status-payment|order|project|project-documents|job-stages|materials|material-type|status-material|delivery|delivery-place|departure-place|status-delivery)>' => '<module>/<controller>',
                 'OPTIONS /<controller:\w*>' => 'site/options',
                 'OPTIONS /<controller:\w+>/<action:\w*>' => 'site/options',
 
