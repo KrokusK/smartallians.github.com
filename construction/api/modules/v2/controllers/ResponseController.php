d12b3e27e1d09ea8f8cb766472dbd125553e19ea
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-02-01 20:06:31 +0600

    Update ResponseController.php

--- construction/api/modules/v2/controllers/ResponseController.php
+++ construction/api/modules/v2/controllers/ResponseController.php
@@ -174,7 +174,6 @@ class ResponseController extends Controller
                 'description' => $modelResponse[0]['description'],
                 'cost' => $modelResponse[0]['cost'],
                 'period' => $modelResponse[0]['period'],
-                'period' => $modelResponse[0]['period'],
                 'profile_id' => $modelResponse[0]['profiles']['id'],
                 'fio' => $modelResponse[0]['profiles']['fio'],
                 'firm_name' => $modelResponse[0]['profiles']['firm_name'],
@@ -210,13 +209,12 @@ class ResponseController extends Controller
                 'description' => $modelResponse[0]['description'],
                 'cost' => $modelResponse[0]['cost'],
                 'period' => $modelResponse[0]['period'],
-                'period' => $modelResponse[0]['period'],
-                'profile_id' => $modelResponse[0]['profiles']['id'],
-                'fio' => $modelResponse[0]['profiles']['fio'],
-                'firm_name' => $modelResponse[0]['profiles']['firm_name'],
-                'avatar' => $modelResponse[0]['profiles']['avatar']
+                //'profile_id' => $modelResponse[0]['profiles']['id'],
+                //'fio' => $modelResponse[0]['profiles']['fio'],
+                //'firm_name' => $modelResponse[0]['profiles']['firm_name'],
+                //'avatar' => $modelResponse[0]['profiles']['avatar']
             );
-            //array_push($RequestResponse, ArrayHelper::toArray($modelResponse));
+            array_push($RequestResponse, ArrayHelper::toArray($modelResponse));
 
             return Json::encode($RequestResponse);
         }
