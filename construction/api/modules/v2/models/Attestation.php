6fda7a63e5c75cba2e0f1995e93d22592c1ce075
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-02-05 17:44:03 +0600

    psr

--- construction/api/modules/v2/models/Attestation.php
+++ construction/api/modules/v2/models/Attestation.php
@@ -3,6 +3,9 @@ namespace api\modules\v2\models;
 
 use Yii;
 use yii\db\ActiveRecord;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\base\InvalidArgumentException;
 
 /**
  * This is the model class for table "attestation".
@@ -10,6 +13,23 @@ use yii\db\ActiveRecord;
  */
 class Attestation extends \yii\db\ActiveRecord
 {
+    /**
+     * Because the field names may match within a single query,
+     * the parameter names may not match the table field names.
+     * To solve this problem let's create an associative array
+     */
+    protected $assocAttestation = [
+        'id' => 'id',
+        'name' => 'name',
+        'limitRec' => 'limit',
+        'offsetRec' => 'offset'
+    ];
+
+    /**
+     * properties
+     */
+    protected $modelResponseMessage;
+
     /**
      * {@inheritdoc}
      */
@@ -23,13 +43,35 @@ class Attestation extends \yii\db\ActiveRecord
      */
     public function rules()
     {
-
         return [
-            [['name'], 'required', 'message' => 'Поле должно быть заполнено'],
-            [['id'], 'match', 'pattern' => '/^[0-9]*$/', 'message' => 'поле id  должно быть типа integer', 'skipOnEmpty' => true],
-            [['name'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
+            [
+                ['name'],
+                'required',
+                'message' => 'Поле должно быть заполнено'
+            ],
+            [
+                ['id'],
+                'match',
+                'pattern' => '/^[0-9]*$/',
+                'message' => 'поле id  должно быть типа integer',
+                'skipOnEmpty' => true
+            ],
+            [
+                ['name'],
+                'string',
+                'max' => 255,
+                'message' => 'Число знаков не должно превышать 255'
+            ],
         ];
+    }
 
+    /**
+     * Create a model
+     */
+    public function __construct()
+    {
+        // Set property
+        $this->modelResponseMessage = new ResponseMessage();
     }
 
     /**
@@ -41,4 +83,375 @@ class Attestation extends \yii\db\ActiveRecord
         return $this->hasMany(Contractor::className(), ['id' => 'contractor_id'])
             ->viaTable('contractor_attestation', ['attestation_id' => 'id']);
     }
+
+    /**
+     * Get Attestation object properties by request params
+     *
+     * @params parameters for filtering
+     *
+     * @throws InvalidArgumentException if data not found or parameters is not validated
+     */
+    public function getDataAttestation($params = [])
+    {
+        // Search data
+        $query = Attestation::find();
+        // Add data filter
+        $this->setDataFilter($query, $params);
+        // Add pagination params
+        $this->setPaginationParams($query, $params);
+        // get data
+        $dataAttestation = $query->orderBy('name')
+            ->asArray()
+            ->all();
+
+        // return data
+        if (!empty($dataAttestation)) {
+            $this->modelResponseMessage->saveArrayMessage(ArrayHelper::toArray($dataAttestation));
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Записи не найденоы');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Set data filter
+     *
+     * @params parameters for filtering
+     * @query object with data filter
+     *
+     * @throws InvalidArgumentException if data not found or parameters is not validated
+     */
+    private function setDataFilter($query, $params = [])
+    {
+        foreach ($this->assocAttestation as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name)) {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+                $query->andWhere([$name => $params[$value]]);
+            }
+        }
+    }
+
+    /**
+     * Set pagination params
+     *
+     * @params parameters for pagination
+     * @query object with data filter
+     */
+    private function setPaginationParams($query, $params = [])
+    {
+        // default values
+        $defauftParams = [
+            'limitRec' => 10,
+            'offsetRec' => 0
+        ];
+
+        foreach ($this->assocAttestation as $name => $value) {
+            switch ($name) {
+                case 'limitRec':
+                    if (array_key_exists($value, $params) && preg_match("/^[0-9]*$/",$params[$value])) {
+                        $query->limit($params[$value]);
+                    } else {
+                        // default value
+                        $query->limit($defauftParams[$name]);
+                    }
+                    break;
+                case 'offsetRec':
+                    if (array_key_exists($value, $params) && preg_match("/^[0-9]*$/",$params[$value])) {
+                        $query->offset($params[$value]);
+                    } else {
+                        // default value
+                        $query->offset($defauftParams[$name]);
+                    }
+            }
+        }
+    }
+
+    /**
+     * Set Attestation properties and
+     * save object into the Db
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function addDataAttestation($params = [])
+    {
+        // fill in the properties in the Attestation object
+        foreach ($this->assocAttestation as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name) && $name != 'id') {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+        }
+
+        return $this->saveDataObject();
+    }
+
+    /**
+     * Save Attestation object
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function saveDataObject()
+    {
+        if ($this->validate()) {
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                $flagAttestation = $this->save(false); // insert into Attestation table
+
+                if ($flagAttestation == true) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Подтверждение не может быть сохранено');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            } catch (Exception $ex) {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Подтверждение не может быть сохранено');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Подтверждение успешно сохранено');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка валидации');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Get Attestation object by id
+     *
+     * @params parameters with properties
+     * @notEmpty check on empty
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function getDataAttestationById($params = [])
+    {
+        if (array_key_exists($this->assocAttestation['id'], $params)) {
+            // check id parametr
+            if (!preg_match("/^[0-9]*$/", $params[$this->assocAttestation['id']])) {
+                $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: id');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            // Search record by id in the database
+            $queryAttestation = Attestation::find()->where(['id' => $params[$this->assocAttestation['id']]]);
+            $modelAttestation = $queryAttestation->one();
+            if (empty($modelAttestation)) {
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: В БД не найдено Подтверждение по id');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            return $modelAttestation;
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Отсутствет id параметр в запросе');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Set Attestation properties and
+     * update object into the Db by id
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function updateDataAttestation($params = [])
+    {
+        // fill in the properties in the Attestation object
+        foreach ($this->assocAttestation as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name) && $name != 'id') {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+        }
+
+        return $this->updateDataObject();
+    }
+
+    /**
+     * Update Attestation object
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function updateDataObject()
+    {
+        // Update Attestation object
+        if ($this->validate()) {
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                $flagAttestation = $this->save(false); // update Attestation table
+
+                if ($flagAttestation) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Подтверждение не может быть обновлено');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            } catch (Exception $ex) {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Подтверждение не может быть обновлено');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Подтверждение успешно сохранено');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка валидации');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Check Id in params. Is this null?
+     *
+     * @params parameters with properties
+     *
+     * @bool return true if id is null
+     */
+    public function isNullIdInParams($params = [])
+    {
+        if (array_key_exists($this->assocAttestation['id'], $params)
+            && !empty($params[$this->assocAttestation['id']])) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Check other parameters in addition to the token
+     *
+     * @params parameters with properties
+     *
+     * @bool return true if id is null
+     */
+    public function isOtherParams($params = [])
+    {
+        if (array_key_exists('token',$params)
+            && count($params) > 1) {
+            $flag = false;
+            foreach ($this->assocAttestation as $name => $value) {
+                if (array_key_exists($value, $params) && $this->hasAttribute($name)) {
+                    $flag = true;
+                }
+            }
+
+            return $flag;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Delete Attestation object into the Db by id
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function deleteDataAttestationById($params = [])
+    {
+        $transaction = \Yii::$app->db->beginTransaction();
+        try {
+            // delete from Attestation table
+            $countAttestationDelete = $this->delete($this->id);
+
+            if ($countAttestationDelete > 0) {
+                $transaction->commit();
+            } else {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Подтверждение не может быть удалено');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+        } catch (Exception $ex) {
+            $transaction->rollBack();
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Подтверждение не может быть удалено');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+
+        $this->modelResponseMessage->saveSuccessMessage('Подтверждение успешно удалено');
+        return Json::encode($this->modelResponseMessage->getDataMessage());
+    }
+
+    /**
+     * Delete Attestation objects into the Db by params
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function deleteDataAttestationByParams($params = [])
+    {
+        if (!$this->isOtherParams($params)) {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Отсутствуют параметры для фильтра');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+
+        // Search records by params in the database
+        $query = Attestation::find();
+        // Add data filter
+        $this->setDataFilter($query, $params);
+        // Add pagination params
+        $this->setPaginationParams($query, $params);
+        // get data
+        $dataAttestation = $query->orderBy('id')->all();
+        // delete records from database
+        return $this->deleteDataAttestationArray($dataAttestation);
+    }
+
+    /**
+     * Delete Attestation objects into the Db by params
+     *
+     * @dataAttestation array of objects
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function deleteDataAttestationArray($dataAttestation)
+    {
+        if (!empty($dataAttestation)) {
+            foreach ($dataAttestation as $dataRec) {
+                $transaction = \Yii::$app->db->beginTransaction();
+                try {
+                    // delete from Attestation table.
+                    $countAttestationDelete = $dataRec->delete();
+
+                    if ($countAttestationDelete > 0) {
+                        $transaction->commit();
+                    } else {
+                        $transaction->rollBack();
+                        $this->modelResponseMessage->saveErrorMessage('Ошибка: Подтверждение не может быть удалено');
+                        throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                    }
+                } catch (Exception $ex) {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Подтверждение не может быть удалено');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Подтверждение успешно удалено');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: В БД не найдено Подтверждение по параметрам');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
 }
+
