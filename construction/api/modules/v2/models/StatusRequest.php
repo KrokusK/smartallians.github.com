b9db4c6563c268dc20febee37e4c18e0315d30ea
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-02-05 16:49:56 +0600

    psr

--- construction/api/modules/v2/models/StatusRequest.php
+++ construction/api/modules/v2/models/StatusRequest.php
@@ -3,6 +3,9 @@ namespace api\modules\v2\models;
 
 use Yii;
 use yii\db\ActiveRecord;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\base\InvalidArgumentException;
 
 /**
  * This is the model class for table "status_request".
@@ -10,6 +13,23 @@ use yii\db\ActiveRecord;
  */
 class StatusRequest extends \yii\db\ActiveRecord
 {
+    /**
+     * Because the field names may match within a single query,
+     * the parameter names may not match the table field names.
+     * To solve this problem let's create an associative array
+     */
+    protected $assocStatusRequest = [
+        'id' => 'id',
+        'name' => 'name',
+        'limitRec' => 'limit',
+        'offsetRec' => 'offset'
+    ];
+
+    /**
+     * properties
+     */
+    protected $modelResponseMessage;
+
     /**
      * {@inheritdoc}
      */
@@ -23,13 +43,35 @@ class StatusRequest extends \yii\db\ActiveRecord
      */
     public function rules()
     {
-
         return [
-            [['name'], 'required', 'message' => 'Поле должно быть заполнено'],
-            [['id'], 'match', 'pattern' => '/^[0-9]*$/', 'message' => 'поле id  должно быть типа integer', 'skipOnEmpty' => true],
-            [['name'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
+            [
+                ['name'],
+                'required',
+                'message' => 'Поле должно быть заполнено'
+            ],
+            [
+                ['id'],
+                'match',
+                'pattern' => '/^[0-9]*$/',
+                'message' => 'поле id  должно быть типа integer',
+                'skipOnEmpty' => true
+            ],
+            [
+                ['name'],
+                'string',
+                'max' => 255,
+                'message' => 'Число знаков не должно превышать 255'
+            ],
         ];
+    }
 
+    /**
+     * Create a model
+     */
+    public function __construct()
+    {
+        // Set property
+        $this->modelResponseMessage = new ResponseMessage();
     }
 
     /**
@@ -40,4 +82,376 @@ class StatusRequest extends \yii\db\ActiveRecord
     {
         return $this->hasOne(Request::className(), ['status_request_id' => 'id']);
     }
+
+    /**
+     * Get StatusRequest object properties by request params
+     *
+     * @params parameters for filtering
+     *
+     * @throws InvalidArgumentException if data not found or parameters is not validated
+     */
+    public function getDataStatusRequest($params = [])
+    {
+        // Search data
+        $query = StatusRequest::find();
+        // Add data filter
+        $this->setDataFilter($query, $params);
+        // Add pagination params
+        $this->setPaginationParams($query, $params);
+        // get data
+        $dataStatusRequest = $query->orderBy('name')
+            ->asArray()
+            ->all();
+
+        // return data
+        if (!empty($dataStatusRequest)) {
+            $this->modelResponseMessage->saveArrayMessage(ArrayHelper::toArray($dataStatusRequest));
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Записи не найдены');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Set data filter
+     *
+     * @params parameters for filtering
+     * @query object with data filter
+     *
+     * @throws InvalidArgumentException if data not found or parameters is not validated
+     */
+    private function setDataFilter($query, $params = [])
+    {
+        foreach ($this->assocStatusRequest as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name)) {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+                $query->andWhere([$name => $params[$value]]);
+            }
+        }
+    }
+
+    /**
+     * Set pagination params
+     *
+     * @params parameters for pagination
+     * @query object with data filter
+     */
+    private function setPaginationParams($query, $params = [])
+    {
+        // default values
+        $defauftParams = [
+            'limitRec' => 10,
+            'offsetRec' => 0
+        ];
+
+        foreach ($this->assocStatusRequest as $name => $value) {
+            switch ($name) {
+                case 'limitRec':
+                    if (array_key_exists($value, $params) && preg_match("/^[0-9]*$/",$params[$value])) {
+                        $query->limit($params[$value]);
+                    } else {
+                        // default value
+                        $query->limit($defauftParams[$name]);
+                    }
+                    break;
+                case 'offsetRec':
+                    if (array_key_exists($value, $params) && preg_match("/^[0-9]*$/",$params[$value])) {
+                        $query->offset($params[$value]);
+                    } else {
+                        // default value
+                        $query->offset($defauftParams[$name]);
+                    }
+            }
+        }
+    }
+
+    /**
+     * Set StatusRequest properties and
+     * save object into the Db
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function addDataStatusRequest($params = [])
+    {
+        // fill in the properties in the StatusRequest object
+        foreach ($this->assocStatusRequest as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name) && $name != 'id') {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+        }
+
+        return $this->saveDataObject();
+    }
+
+    /**
+     * Save StatusRequest object
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function saveDataObject()
+    {
+        if ($this->validate()) {
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                $flagStatusRequest = $this->save(false); // insert into StatusRequest table
+
+                if ($flagStatusRequest == true) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Статус заявки не может быть сохранен');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            } catch (Exception $ex) {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Статус заявки не может быть сохранен');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Статус заявки успешно сохранен');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка валидации');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Get StatusRequest object by id
+     *
+     * @params parameters with properties
+     * @notEmpty check on empty
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function getDataStatusRequestById($params = [])
+    {
+        if (array_key_exists($this->assocStatusRequest['id'], $params)) {
+            // check id parametr
+            if (!preg_match("/^[0-9]*$/", $params[$this->assocStatusRequest['id']])) {
+                $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: id');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            // Search record by id in the database
+            $queryStatusRequest = StatusRequest::find()->where(['id' => $params[$this->assocStatusRequest['id']]]);
+            $modelStatusRequest = $queryStatusRequest->one();
+            if (empty($modelStatusRequest)) {
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: В БД не найден Статус заявки по id');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            return $modelStatusRequest;
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Отсутствет id параметр в запросе');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Set StatusRequest properties and
+     * update object into the Db by id
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function updateDataStatusRequest($params = [])
+    {
+        // fill in the properties in the StatusRequest object
+        foreach ($this->assocStatusRequest as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name) && $name != 'id') {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+        }
+
+        return $this->updateDataObject();
+    }
+
+    /**
+     * Update StatusRequest object
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function updateDataObject()
+    {
+        // Update StatusRequest object
+        if ($this->validate()) {
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                $flagStatusRequest = $this->save(false); // update StatusRequest table
+
+                if ($flagStatusRequest) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Статус заявки не может быть обновлен');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            } catch (Exception $ex) {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Статус заявки не может быть обновлен');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Статус заявки успешно сохранен');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка валидации');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Check Id in params. Is this null?
+     *
+     * @params parameters with properties
+     *
+     * @bool return true if id is null
+     */
+    public function isNullIdInParams($params = [])
+    {
+        if (array_key_exists($this->assocStatusRequest['id'], $params)
+            && !empty($params[$this->assocStatusRequest['id']])) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Check other parameters in addition to the token
+     *
+     * @params parameters with properties
+     *
+     * @bool return true if id is null
+     */
+    public function isOtherParams($params = [])
+    {
+        if (array_key_exists('token',$params)
+            && count($params) > 1) {
+            $flag = false;
+            foreach ($this->assocStatusRequest as $name => $value) {
+                if (array_key_exists($value, $params) && $this->hasAttribute($name)) {
+                    $flag = true;
+                }
+            }
+
+            return $flag;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Delete StatusRequest object into the Db by id
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function deleteDataStatusRequestById($params = [])
+    {
+        $transaction = \Yii::$app->db->beginTransaction();
+        try {
+            // delete from StatusRequest table
+            $countStatusRequestDelete = $this->delete($this->id);
+
+            if ($countStatusRequestDelete > 0) {
+                $transaction->commit();
+            } else {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Статус заявки не может быть удален');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+        } catch (Exception $ex) {
+            $transaction->rollBack();
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Статус заявки не может быть удален');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+
+        $this->modelResponseMessage->saveSuccessMessage('Статус заявки успешно удален');
+        return Json::encode($this->modelResponseMessage->getDataMessage());
+    }
+
+    /**
+     * Delete StatusRequest objects into the Db by params
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function deleteDataStatusRequestByParams($params = [])
+    {
+        if (!$this->isOtherParams($params)) {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Отсутствуют параметры для фильтра');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+
+        // Search records by params in the database
+        $query = StatusRequest::find();
+        // Add data filter
+        $this->setDataFilter($query, $params);
+        // Add pagination params
+        $this->setPaginationParams($query, $params);
+        // get data
+        $dataStatusRequest = $query->orderBy('id')->all();
+        // delete records from database
+        return $this->deleteDataStatusRequestArray($dataStatusRequest);
+    }
+
+    /**
+     * Delete StatusRequest objects into the Db by params
+     *
+     * @dataStatusRequest array of objects
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function deleteDataStatusRequestArray($dataStatusRequest)
+    {
+        if (!empty($dataStatusRequest)) {
+            foreach ($dataStatusRequest as $dataRec) {
+                $transaction = \Yii::$app->db->beginTransaction();
+                try {
+                    // delete from StatusRequest table.
+                    $countStatusRequestDelete = $dataRec->delete();
+
+                    if ($countStatusRequestDelete > 0) {
+                        $transaction->commit();
+                    } else {
+                        $transaction->rollBack();
+                        $this->modelResponseMessage->saveErrorMessage('Ошибка: Статус заявки не может быть удален');
+                        throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                    }
+                } catch (Exception $ex) {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Статус заявки не может быть удален');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Статус заявки успешно удален');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: В БД не найден Статус заявки по параметрам');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
 }
+
+
