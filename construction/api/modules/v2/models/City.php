7d4c7d48d180460cd20423fa528b9568a06434ba
KrokusK <55874907+KrokusK@users.noreply.github.com>
2020-02-06 15:15:21 +0600

    psr city

--- construction/api/modules/v2/models/City.php
+++ construction/api/modules/v2/models/City.php
@@ -3,19 +3,40 @@ namespace api\modules\v2\models;
 
 use Yii;
 use yii\db\ActiveRecord;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\base\InvalidArgumentException;
 
 /**
- * This is the model class for table "city".
+ * This is the model class for table "region".
  *
  */
 class City extends \yii\db\ActiveRecord
 {
     /**
+     * Because the field names may match within a single query,
+     * the parameter names may not match the table field names.
+     * To solve this problem let's create an associative array
+     */
+    protected $assocCity = [
+        'id' => 'id',
+        'region_id' => 'region_id',
+        'name' => 'name',
+        'limitRec' => 'limit',
+        'offsetRec' => 'offset'
+    ];
+
+    /**
+     * properties
+     */
+    protected $modelResponseMessage;
+
+    /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
-        return '{{%city}}';
+        return '{{%region}}';
     }
 
     /**
@@ -23,23 +44,41 @@ class City extends \yii\db\ActiveRecord
      */
     public function rules()
     {
-
         return [
-            [['name', 'region_id'], 'required', 'message' => 'Поле должно быть заполнено'],
-            [['id'], 'match', 'pattern' => '/^[0-9]*$/', 'message' => 'поле id  должно быть типа integer', 'skipOnEmpty' => true],
-            [['region_id'], 'in', 'range' =>
-                function ( $attribute, $params ) {
-                    $statusesRegionId = Region::find()->select(['id'])->asArray()->all();
-                    $statusesRegionIdStr = [];
-                    foreach ($statusesRegionId as $item) {
-                        array_push($statusesRegionIdStr, "{$item['id']}");
-                    }
-                    return $statusesRegionIdStr;
-                },
-                'message' => 'Регион не выбран из списка'],
-            [['name'], 'string', 'max' => 255, 'message' => 'Число знаков не должно превышать 255'],
+            [
+                ['name','region_id'],
+                'required',
+                'message' => 'Поле должно быть заполнено'
+            ],
+            [
+                ['id'],
+                'match',
+                'pattern' => '/^[0-9]*$/',
+                'message' => 'поле id  должно быть типа integer',
+                'skipOnEmpty' => true
+            ],
+            [
+                ['region_id'],
+                'match',
+                'pattern' => '/^[0-9]*$/',
+                'message' => 'поле id  должно быть типа integer',
+            ],
+            [
+                ['name'],
+                'string',
+                'max' => 255,
+                'message' => 'Число знаков не должно превышать 255'
+            ],
         ];
+    }
 
+    /**
+     * Create a model
+     */
+    public function __construct()
+    {
+        // Set property
+        $this->modelResponseMessage = new ResponseMessage();
     }
 
     /**
@@ -86,5 +125,375 @@ class City extends \yii\db\ActiveRecord
     public function getDeparturePlace()
     {
         return $this->hasOne(DeparturePlace::className(), ['city_id' => 'id']);
+    }
+
+    /**
+     * Get City object properties by request params
+     *
+     * @params parameters for filtering
+     *
+     * @throws InvalidArgumentException if data not found or parameters is not validated
+     */
+    public function getDataCity($params = [])
+    {
+        // Search data
+        $query = City::find();
+        // Add data filter
+        $this->setDataFilter($query, $params);
+        // Add pagination params
+        $this->setPaginationParams($query, $params);
+        // get data
+        $dataCity = $query->orderBy('name')
+            ->asArray()
+            ->all();
+
+        // return data
+        if (!empty($dataCity)) {
+            $this->modelResponseMessage->saveArrayMessage(ArrayHelper::toArray($dataCity));
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Записи не найдены');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Set data filter
+     *
+     * @params parameters for filtering
+     * @query object with data filter
+     *
+     * @throws InvalidArgumentException if data not found or parameters is not validated
+     */
+    private function setDataFilter($query, $params = [])
+    {
+        foreach ($this->assocCity as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name)) {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+                $query->andWhere([$name => $params[$value]]);
+            }
+        }
+    }
+
+    /**
+     * Set pagination params
+     *
+     * @params parameters for pagination
+     * @query object with data filter
+     */
+    private function setPaginationParams($query, $params = [])
+    {
+        // default values
+        $defauftParams = [
+            'limitRec' => 10,
+            'offsetRec' => 0
+        ];
+
+        foreach ($this->assocCity as $name => $value) {
+            switch ($name) {
+                case 'limitRec':
+                    if (array_key_exists($value, $params) && preg_match("/^[0-9]*$/",$params[$value])) {
+                        $query->limit($params[$value]);
+                    } else {
+                        // default value
+                        $query->limit($defauftParams[$name]);
+                    }
+                    break;
+                case 'offsetRec':
+                    if (array_key_exists($value, $params) && preg_match("/^[0-9]*$/",$params[$value])) {
+                        $query->offset($params[$value]);
+                    } else {
+                        // default value
+                        $query->offset($defauftParams[$name]);
+                    }
+            }
+        }
+    }
+
+    /**
+     * Set City properties and
+     * save object into the Db
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function addDataCity($params = [])
+    {
+        // fill in the properties in the City object
+        foreach ($this->assocCity as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name) && $name != 'id') {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+        }
+
+        return $this->saveDataObject();
+    }
+
+    /**
+     * Save City object
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function saveDataObject()
+    {
+        if ($this->validate()) {
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                $flagCity = $this->save(false); // insert into City table
+
+                if ($flagCity == true) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Город не может быть сохранен');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            } catch (Exception $ex) {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Город не может быть сохранен');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Город успешно сохранен');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка валидации');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Get City object by id
+     *
+     * @params parameters with properties
+     * @notEmpty check on empty
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function getDataCityById($params = [])
+    {
+        if (array_key_exists($this->assocCity['id'], $params)) {
+            // check id parametr
+            if (!preg_match("/^[0-9]*$/", $params[$this->assocCity['id']])) {
+                $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: id');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            // Search record by id in the database
+            $queryCity = City::find()->where(['id' => $params[$this->assocCity['id']]]);
+            $modelCity = $queryCity->one();
+            if (empty($modelCity)) {
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: В БД не найден Город по id');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            return $modelCity;
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Отсутствет id параметр в запросе');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Set City properties and
+     * update object into the Db by id
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function updateDataCity($params = [])
+    {
+        // fill in the properties in the City object
+        foreach ($this->assocCity as $name => $value) {
+            if (array_key_exists($value, $params) && $this->hasAttribute($name) && $name != 'id') {
+                $this->$name = $params[$value];
+                if (!$this->validate($name)) {
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка валидации: параметр ' . $value);
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+        }
+
+        return $this->updateDataObject();
+    }
+
+    /**
+     * Update City object
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function updateDataObject()
+    {
+        // Update City object
+        if ($this->validate()) {
+            $transaction = \Yii::$app->db->beginTransaction();
+            try {
+                $flagCity = $this->save(false); // update City table
+
+                if ($flagCity) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Город не может быть обновлен');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            } catch (Exception $ex) {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Город не может быть обновлен');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Город успешно сохранен');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка валидации');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+    }
+
+    /**
+     * Check Id in params. Is this null?
+     *
+     * @params parameters with properties
+     *
+     * @bool return true if id is null
+     */
+    public function isNullIdInParams($params = [])
+    {
+        if (array_key_exists($this->assocCity['id'], $params)
+            && !empty($params[$this->assocCity['id']])) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Check other parameters in addition to the token
+     *
+     * @params parameters with properties
+     *
+     * @bool return true if id is null
+     */
+    public function isOtherParams($params = [])
+    {
+        if (array_key_exists('token',$params)
+            && count($params) > 1) {
+            $flag = false;
+            foreach ($this->assocCity as $name => $value) {
+                if (array_key_exists($value, $params) && $this->hasAttribute($name)) {
+                    $flag = true;
+                }
+            }
+
+            return $flag;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Delete City object into the Db by id
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function deleteDataCityById($params = [])
+    {
+        $transaction = \Yii::$app->db->beginTransaction();
+        try {
+            // delete from City table
+            $countCityDelete = $this->delete($this->id);
+
+            if ($countCityDelete > 0) {
+                $transaction->commit();
+            } else {
+                $transaction->rollBack();
+                $this->modelResponseMessage->saveErrorMessage('Ошибка: Город не может быть удален');
+                throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+            }
+        } catch (Exception $ex) {
+            $transaction->rollBack();
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Город не может быть удален');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+
+        $this->modelResponseMessage->saveSuccessMessage('Город успешно удален');
+        return Json::encode($this->modelResponseMessage->getDataMessage());
+    }
+
+    /**
+     * Delete City objects into the Db by params
+     *
+     * @params parameters with properties
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    public function deleteDataCityByParams($params = [])
+    {
+        if (!$this->isOtherParams($params)) {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: Отсутствуют параметры для фильтра');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
+
+        // Search records by params in the database
+        $query = City::find();
+        // Add data filter
+        $this->setDataFilter($query, $params);
+        // Add pagination params
+        $this->setPaginationParams($query, $params);
+        // get data
+        $dataCity = $query->orderBy('id')->all();
+        // delete records from database
+        return $this->deleteDataCityArray($dataCity);
+    }
+
+    /**
+     * Delete City objects into the Db by params
+     *
+     * @dataCity array of objects
+     *
+     * @throws InvalidArgumentException if returned error
+     */
+    private function deleteDataCityArray($dataCity)
+    {
+        if (!empty($dataCity)) {
+            foreach ($dataCity as $dataRec) {
+                $transaction = \Yii::$app->db->beginTransaction();
+                try {
+                    // delete from City table.
+                    $countCityDelete = $dataRec->delete();
+
+                    if ($countCityDelete > 0) {
+                        $transaction->commit();
+                    } else {
+                        $transaction->rollBack();
+                        $this->modelResponseMessage->saveErrorMessage('Ошибка: Город не может быть удален');
+                        throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                    }
+                } catch (Exception $ex) {
+                    $transaction->rollBack();
+                    $this->modelResponseMessage->saveErrorMessage('Ошибка: Город не может быть удален');
+                    throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+                }
+            }
+
+            $this->modelResponseMessage->saveSuccessMessage('Город успешно удален');
+            return Json::encode($this->modelResponseMessage->getDataMessage());
+        } else {
+            $this->modelResponseMessage->saveErrorMessage('Ошибка: В БД не найден Город по параметрам');
+            throw new InvalidArgumentException(Json::encode($this->modelResponseMessage->getErrorMessage()));
+        }
     }
 }
